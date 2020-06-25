cf9e57f4e9fa9593a96ca74a4f0c7092eaa5191a
hyo-ha <hyo.maeda@di-v.co.jp>
2020-06-25 23:37:15 +0900

    add-message

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,8 @@
 class GroupsController < ApplicationController
 
-  def index
+  # def index
     
-  end
+  # end
 
   def new
     @group = Group.new
@@ -25,15 +25,20 @@ class GroupsController < ApplicationController
   def update
     @group = Group.find(params[:id])
     if @group.update(group_params)
-      redirect_to root_path, notice: 'グループを更新しました'
+      redirect_to group_messages_path(@group), notice: 'グループを更新しました'
     else
       render :edit
     end
   end
 
   private
+
   def group_params
     params.require(:group).permit(:name, user_ids: [])
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 
 end
